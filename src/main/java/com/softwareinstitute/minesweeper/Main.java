dfeaddb6fb1780d8ecacd5e5e1e9469991f43e07
Maria <mariacjones1@hotmail.co.uk>
2023-03-23 09:53:50 +0000

    Set game to end when user clicks on a mine or has found all empty spaces

--- src/main/java/com/softwareinstitute/minesweeper/Main.java
+++ src/main/java/com/softwareinstitute/minesweeper/Main.java
@@ -18,18 +18,24 @@ public class Main {
         List<Point> flags = new ArrayList<>();
 
         Boolean runGame = true;
+        Boolean winner = null;
 
-        do {
+        while (runGame) {
             System.out.println("Choose a column 0-" + (board.width - 1));
             int col = chooseCol.nextInt();
             System.out.println("Choose a row 0-" + (board.length - 1));
             int row = chooseRow.nextInt();
             System.out.println("Do you want to click (C) or place a flag (F)?");
             String click = chooseClick.nextLine().toUpperCase();
+            Point selection = new Point(row, col);
             if (click.equals("C")) {
-                clicks.add(new Point(row, col));
+                clicks.add(selection);
+                if (mines.contains(selection)) {
+                    runGame = false;
+                    winner = false;
+                }
             } else if (click.equals("F")) {
-                flags.add(new Point(row, col));
+                flags.add(selection);
             } else {
                 System.out.println("Invalid choice.");
             }
@@ -37,7 +43,18 @@ public class Main {
             System.out.println(flags);
 
             board.printUpdatedBoard(clicks, flags, mines);
-        } while (runGame);
+            if (clicks.size() == (board.length * board.width - mines.size())) {
+                runGame = false;
+                winner = true;
+                break;
+            }
+        }
+
+        if (winner == true) {
+            System.out.println("You win!");
+        } else {
+            System.out.println("Game over!");
+        }
     }
 
 
